e671f508c2fc3a7e488cbf9d0472b74caec6034f
hyochan <dooboolab@gmail.com>
2019-02-24 23:33:03 +0900

    Enhanced workaround for Localized widget test
    Basically, pass `isTest` param to `Localization` class when running the test.

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -1,32 +1,50 @@
 import 'package:flutter/material.dart';
-
+import '../utils/theme.dart' as Theme;
 import '../utils/localization.dart';
-import '../utils/general.dart';
+import '../widgets/button.dart' show Button;
 
-class Intro extends StatefulWidget {
-  Intro({Key key}) : super(key: key);
-
-  @override
-  IntroState createState() => new IntroState();
-}
-
-class IntroState extends State<Intro> {
+class Intro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var localization = Localization.of(context);
+    var icBooKoo = AssetImage("res/icons/BooKoo.png");
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'dooboolab'
+      body: Container(
+        child: SafeArea(
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverPadding(
+                padding: const EdgeInsets.only(top: 168.0),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate(
+                    <Widget>[
+                      Image(image: icBooKoo, width: 200.0, height: 60.0),
+                      Button(
+                        margin: EdgeInsets.only(top: 200.0, left: 60.0, right: 60.0),
+                        fontSize: 18.0,
+                        text: localization.trans('LOGIN'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        constraints: BoxConstraints.expand(
+          height: double.infinity,
+          width: double.infinity,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Theme.Colors.dusk, Theme.Colors.darkGray],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
         ),
       ),
-      body: Center(
-//        child: Text(localization.trans('INTRO')),
-      ),
     );
-  }
-
-  _navigate(String path) {
-    General.instance.navigateScreenNamed(context, path, reset: false);
   }
 }
--- test/intro_test.dart
+++ test/intro_test.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'package:bookoo2/screens/intro.dart';
+import 'package:bookoo2/screens/intro.dart' show Intro;
 import './test_utils.dart' show TestUtils;
+import '../lib/utils/localization.dart' show Localization;
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  testWidgets('Widget', (WidgetTester tester) async {
+  testWidgets("Widget", (WidgetTester tester) async{
     await tester.pumpWidget(TestUtils.makeTestableWidget(child: Intro()));
     await tester.pumpAndSettle();
 
-    expect(find.text('dooboolab'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    print(tester.widgetList(find.byType(Text)));
-
-    var finderByType = find.byType(Text);
-    print(finderByType.evaluate());
+    var findByText = find.byType(Text);
+    print(findByText.evaluate());
+    expect(findByText.evaluate().isEmpty, false);
+    expect(find.text('LOGIN'), findsOneWidget);
   });
 }
