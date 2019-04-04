8bd0af440e4642536a5221c20de1a49431bf0f54
hyochan <dooboolab@gmail.com>
2019-04-04 17:58:42 +0900

    Wrap drawer with safe area view

--- lib/screens/my_drawer.dart
+++ lib/screens/my_drawer.dart
@@ -9,30 +9,31 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(top: 24.0),
-          padding: EdgeInsets.symmetric(horizontal: 8.0),
-          width: double.infinity,
-          height: 56.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.close),
-                iconSize: 24.0,
-                onPressed: onClose,
-              ),
-              IconButton(
-                icon: Icon(Icons.settings),
-                iconSize: 24.0,
-                onPressed: onSetting,
-              ),
-            ],
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            width: double.infinity,
+            height: 56.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.close),
+                  iconSize: 24.0,
+                  onPressed: onClose,
+                ),
+                IconButton(
+                  icon: Icon(Icons.settings),
+                  iconSize: 24.0,
+                  onPressed: onSetting,
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
