3909960ef815edfdfc4bf45527f742ec96488273
Gordon Freeman <godon019@gmail.com>
2019-09-21 15:03:24 +0900

    Add test

--- test/screens/home_calendar_test.dart
+++ test/screens/home_calendar_test.dart
@@ -1,10 +1,26 @@
 
-import 'package:bookoo2/screens/home_calendar.dart';
+import 'package:bookoo2/screens/home_calendar.dart' show HomeCalendar;
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../test_utils.dart' show TestUtils;
+
 void main() {
-  testWidgets('Widget', (WidgetTester tester) async {
-    await tester.pumpWidget(HomeCalendar());
+  testWidgets('Show [Ledgers] on the date when the corresponding date has been clicked', (WidgetTester tester) async {
+    await tester.pumpWidget(TestUtils.makeTestableWidget(child: HomeCalendar()));
     await tester.pumpAndSettle();
+
+     var findByText = find.byType(Text);
+    expect(findByText.evaluate().isEmpty, false);
+
+    expect(find.text('10'), findsNWidgets(1));
+
+    await tester.tap(find.text('10'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('운동'), findsOneWidget);
+    expect(find.text('용돈'), findsOneWidget);
+    expect(find.text('데이트'), findsOneWidget);
+    expect(find.text('커피'), findsNWidgets(4));
   });
 }
