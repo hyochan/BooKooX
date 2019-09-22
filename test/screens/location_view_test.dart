5ee522e4f8b51aee6d1e46b854d1d5ed8aae1fed
hyochan <dooboolab@gmail.com>
2019-09-22 18:01:47 +0900

    Add `location_view_test.dart`

---
+++ test/screens/location_view_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:bookoo2/screens/location_view.dart' show LocationView;
+import '../test_utils.dart' show TestUtils;
+
+void main() {
+  testWidgets("Widget", (WidgetTester tester) async{
+    await tester.pumpWidget(TestUtils.makeTestableWidget(child: LocationView()));
+    await tester.pumpAndSettle();
+
+    var findByType = find.byType(Container);
+    expect(findByType.evaluate().isEmpty, false);
+
+    expect(find.byType(RawMaterialButton), findsNWidgets(3));
+  });
+}
