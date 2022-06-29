45f902a3f685cb4e554bc85f79cf7ac48e71b0e6
Jay-flow <kimpetro153@gmail.com>
2022-06-29 12:53:10 +0900

    Fix the locale in the seed

--- firebase/functions/src/seeds/index.ts
+++ firebase/functions/src/seeds/index.ts
@@ -1,9 +1,10 @@
 import { faker } from '@faker-js/faker'
 import init from '../utils/auth'
 import insertUsers from './user'
+import 'dotenv/config'
 
 const configSeed = async () => {
-  faker.setLocale('ko')
+  faker.setLocale(process.env.FAKER_LOCALE ?? 'en')
   await init()
 }
 
